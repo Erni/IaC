e9edac88610e57ce54dd31e169593ee6e2a0dc48
Erni <ernesto.reig@gmail.com>
2022-11-02 12:28:05 +0100

    finished example of a web server in an ASG behind a LB

--- webserver-cluster/main.tf
+++ webserver-cluster/main.tf
@@ -1,5 +1,8 @@
+# Define AWS Provider #######################################
+# It automatically takes the credentials from
+# "~/.aws/credentials" for profile "okta-elastic-dev"
 provider "aws" {
-  region = "us-east-2"
+  region                   = "us-east-2"
   profile                  = "okta-elastic-dev"
 }
 
@@ -9,7 +12,42 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_security_group" "sg" {
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+################################################################
+# Define the AWS Launch Configuration ##########################
+################################################################
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+################################################################
+# Define Security Groups #######################################
+################################################################
+# SG for the instance in the ASG
+resource "aws_security_group" "instance" {
 
   name = "ernesto-reig-tf-test-sg"
 
@@ -21,7 +59,7 @@ resource "aws_security_group" "sg" {
   }
 }
 
-
+# SG for the Load Balancer
 resource "aws_security_group" "alb" {
 
   name = "sec_group_alb"
@@ -43,18 +81,10 @@ resource "aws_security_group" "alb" {
   }
 }
 
-
-data "aws_vpc" "default-vpc" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default-vpc.id]
-  }
-}
-
+################################################################
+# Define Load Balancer and ASG #################################
+################################################################
+# 1 - Define the LB itself
 resource "aws_lb" "example" {
   name               = "lb"
 
@@ -63,26 +93,68 @@ resource "aws_lb" "example" {
   security_groups    = [aws_security_group.alb.id]
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.sg.id]
+# 2 - Define the LB Listener referencing the previous LB
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  
-  # Required when using a launch configuration with an auto scaling group.
-  lifecycle {
-    create_before_destroy = true
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
   }
 }
 
+# 3 - Define the LB Listener Rule, forwarding to a Target Group
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+# 4 - Define Target Group, listening in var.server_port
+resource "aws_lb_target_group" "asg" {
+
+  name = "terraform-asg-example"
+
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# 5 - Define ASG adding instances to the previous target group
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -92,4 +164,11 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
+}
+# End of Load Balancer definition ##############################
+################################################################
+
+output "name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the Load Balancer"
 }
