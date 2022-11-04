489493d4f60c66963d595183d189dd6b30db32a0
Erni <ernesto.reig@gmail.com>
2022-11-04 07:08:06 +0100

    externalizing sg rules to be used in different root modules

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -52,22 +52,28 @@ resource "aws_security_group" "instance" {
 resource "aws_security_group" "alb" {
 
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+# Allow inbound HTTP requests
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  # Allow all outbound requests
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+    from_port       = local.http_port
+    to_port         = local.http_port
+    protocol        = local.tcp_protocol
+    cidr_blocks     = local.all_ips
+}
+
+# Allow all outbound requests
+resource "aws_security_group_rule" "allow_http_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port         = local.any_port
+  to_port           = local.any_port
+  protocol          = local.any_protocol
+  cidr_blocks       = local.all_ips
 }
 
 ################################################################
